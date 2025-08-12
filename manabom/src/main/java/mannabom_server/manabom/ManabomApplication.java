acf89b7388e7c3ff57e3fda1fcd06aaa677539bc
kimjuneon <juneon0830@naver.com>
2025-08-12 21:28:43 +0900

    Add files via upload

---
+++ manabom/src/main/java/mannabom_server/manabom/ManabomApplication.java
@@ -0,0 +1,13 @@
+package mannabom_server.manabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManabomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManabomApplication.class, args);
+	}
+
+}
---
+++ manabom/src/test/java/mannabom_server/manabom/ManabomApplicationTests.java
@@ -0,0 +1,13 @@
+package mannabom_server.manabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManabomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

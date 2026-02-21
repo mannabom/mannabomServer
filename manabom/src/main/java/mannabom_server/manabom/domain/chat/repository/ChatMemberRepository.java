0376bd09872da42fd5eb5c4acc85180c71f74013
Sehi55 <sehi1028@naver.com>
2026-02-21 20:26:11 +0900

    feat: loveview 프로필 사진 요청 구현

--- manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMemberRepository.java
+++ manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMemberRepository.java
@@ -1,7 +1,11 @@
 package mannabom_server.manabom.domain.chat.repository;
 
 import mannabom_server.manabom.domain.chat.entity.ChatMember;
+import mannabom_server.manabom.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
+    List<ChatMember> findByRoomId(Long roomId);
 }
--- manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMessageRepository.java
+++ manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMessageRepository.java
@@ -3,5 +3,10 @@ package mannabom_server.manabom.domain.chat.repository;
 import mannabom_server.manabom.domain.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
+    int countChatMessagesByRoom_Id(Long roomId);
+
+    int countChatMessagesByRoom_IdAndCreatedAtAfter(Long roomId, Instant after);
 }

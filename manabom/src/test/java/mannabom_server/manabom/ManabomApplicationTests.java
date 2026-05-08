39483cb2de4277926940bcdb9527c5d8cfa056e2
juneon <juneon0830@naver.com>
2026-05-08 14:02:09 +0900

    test: 애플리케이션 테스트 환경 안정화

--- manabom/src/test/java/mannabom_server/manabom/ManabomApplicationTests.java
+++ manabom/src/test/java/mannabom_server/manabom/ManabomApplicationTests.java
@@ -1,10 +1,17 @@
 package mannabom_server.manabom;
 
+import mannabom_server.manabom.application.pushService.service.pushSender.PushSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ManabomApplicationTests {
+
+	@MockitoBean
+	PushSender pushSender;
 
 	@Test
 	void contextLoads() {

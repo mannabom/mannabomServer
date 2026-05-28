97b5437a126755b9c4d4a29bdfae75cf2f2a79c9
Sehi55 <sehi1028@naver.com>
2026-05-28 18:21:32 +0900

    refactor: 쿼리 오류 수정

--- manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMessageRepository.java
+++ manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMessageRepository.java
@@ -24,10 +24,10 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
 
 
 
-    @Query("SELECT m FROM ChatMessage m JOIN FETCH m.user u JOIN Profile p on p.user.userId = u.userId WHERE m.room.id = :roomId AND m.id > :lastMessageId ORDER BY m.id desc ")
+    @Query("SELECT m FROM ChatMessage m JOIN FETCH m.user u JOIN Profile p on p.user.userId = u.userId WHERE m.room.id = :roomId AND m.id > :lastReadId ORDER BY m.id desc ")
     List<ChatMessage> findChatMessagesAfter(@Param("roomId") Long roomId, @Param("lastReadId") Long lastReadId, Pageable pageable);
 
-    @Query("SELECT m FROM ChatMessage m JOIN FETCH m.user u JOIN Profile p on p.user.userId = u.userId WHERE m.room.id = :roomId AND m.id > :firstMessageId ORDER BY m.id desc ")
+    @Query("SELECT m FROM ChatMessage m JOIN FETCH m.user u JOIN Profile p on p.user.userId = u.userId WHERE m.room.id = :roomId AND m.id < :firstMessageId ORDER BY m.id desc ")
     List<ChatMessage> findChatMessagesBefore(@Param("roomId") Long roomId, @Param("firstMessageId") Long firstMessageId, Pageable pageable);
 
 

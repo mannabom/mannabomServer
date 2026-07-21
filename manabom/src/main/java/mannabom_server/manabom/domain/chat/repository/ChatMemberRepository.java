db01a8cd61879ce67830272c205a25e07eb97295
sehi55 <sehi1028@naver.com>
2026-07-21 11:27:49 +0900

    feat: 매칭 성사 후 동성 채팅방 비활성화

--- manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMemberRepository.java
+++ manabom/src/main/java/mannabom_server/manabom/domain/chat/repository/ChatMemberRepository.java
@@ -2,6 +2,7 @@ package mannabom_server.manabom.domain.chat.repository;
 
 import mannabom_server.manabom.domain.chat.entity.ChatMember;
 import mannabom_server.manabom.domain.chat.enums.ChatMemberStatus;
+import mannabom_server.manabom.domain.chat.enums.ChatStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,6 +20,12 @@ public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
     boolean existsByRoomIdAndUser_UserId(Long roomId, Long userId);
 
     List<ChatMember> findAllByUser_UserIdAndStatus(Long userId, ChatMemberStatus status);
+
+    List<ChatMember> findAllByUser_UserIdAndStatusAndRoom_ChatStatus(
+            Long userId,
+            ChatMemberStatus status,
+            ChatStatus chatStatus
+    );
 
     List<ChatMember> findByRoomId(Long roomId);
 
